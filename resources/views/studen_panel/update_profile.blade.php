46593ed9ffefeae115885d17224f127fa6a8e61b
wabbi-khan <kwahab789@gmail.com>
2023-07-21 03:02:12 -0700

    student update profile

--- resources/views/studen_panel/update_profile.blade.php
+++ resources/views/studen_panel/update_profile.blade.php
@@ -45,9 +45,104 @@
   </li>
 </ul>
 <div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">General Information</div>
-  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">Personal Information</div>
-  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">Contact</div>
+  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+     
+  <div class=" pt-4">
+  <table class="table table-bordered table-hover rounded">
+  <tbody>
+    <tr>
+      <th>General Information</th>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <th>First Name</th>
+      <td>John</td>
+      <td>Peter</td>
+      <td>John</td>
+    </tr>
+    <tr>
+      <th>Last Name</th>
+      <td>Carter</td>
+      <td>Parker</td>
+      <td>Rambo</td>
+    </tr>
+    <tr>
+      <th>Email</th>
+      <td>johncarter@mail.com</td>
+      <td>peterparker@mail.com</td>
+      <td>johnrambo@mail.com</td>
+    </tr>
+  </tbody>
+</table>
+  </div> 
+</div>
+  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+  <div class=" pt-4">
+  <table class="table table-bordered table-hover rounded">
+  <tbody>
+    <tr>
+      <th>Personal Information</th>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <th>First Name</th>
+      <td>John</td>
+      <td>Peter</td>
+      <td>John</td>
+    </tr>
+    <tr>
+      <th>Last Name</th>
+      <td>Carter</td>
+      <td>Parker</td>
+      <td>Rambo</td>
+    </tr>
+    <tr>
+      <th>Email</th>
+      <td>johncarter@mail.com</td>
+      <td>peterparker@mail.com</td>
+      <td>johnrambo@mail.com</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+  </div>
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+
+  <div class=" pt-4">
+  <table class="table table-bordered table-hover rounded">
+  <tbody>
+    <tr>
+      <th>Contact</th>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <th>First Name</th>
+      <td>John</td>
+      <td>Peter</td>
+      <td>John</td>
+    </tr>
+    <tr>
+      <th>Last Name</th>
+      <td>Carter</td>
+      <td>Parker</td>
+      <td>Rambo</td>
+    </tr>
+    <tr>
+      <th>Email</th>
+      <td>johncarter@mail.com</td>
+      <td>peterparker@mail.com</td>
+      <td>johnrambo@mail.com</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+  </div>
 </div>
                             </div>
                         </div>
